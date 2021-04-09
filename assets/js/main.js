169069870718b076facd50ea9e2245d04b7282e1
jrarkaan <rakajanitraa@hotmail.com>
2021-04-09 15:08:03 +0700

    inisiasi projek 1

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// menu show
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId);
+   const nav = document.getElementById(navId);
+
+   if(toggle && nav) return toggle.addEventListener('click', ()=>{
+                                    nav.classList.toggle('show')
+                                 });
+}
+showMenu('nav-toggle','nav-menu');
+// active and remove menu
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+   // active link
+   navLink.forEach(n => n.classList.remove('active'));
+   this.classList.add('active');
+
+   // remove menu mobile
+   const navMenu = document.getElementById('nav-menu');
+   navMenu.classList.remove('show');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
